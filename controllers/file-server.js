150d8b56bcbb61213964de048c833adeba8ae8d1
shawon-majid <shawonmajid@gmail.com>
2024-04-30 10:38:56 +0600

    Fixed Image loading issue

--- controllers/file-server.js
+++ controllers/file-server.js
@@ -1,15 +1,46 @@
 const { getFile, deleteFile } = require("../services/aws/file-server");
 const axios = require("axios");
+const fs = require("fs");
+const path = require("path");
 
 const getFileFromAWS = async (req, res) => {
   try {
-    const url = await getFile(req.params.fileName);
+    // check if the file exists, if so then send the file
 
-    const response = await axios.get(url, { responseType: "stream" });
+    const root = __dirname + "/../";
 
-    res.setHeader("Content-Type", response.headers["content-type"]);
+    let filePath = root + req.params.fileName;
+    filePath = path.resolve(filePath);
 
-    response.data.pipe(res);
+    console.log(filePath);
+    if (fs.existsSync(filePath)) {
+      console.log("file exists...");
+
+      console.log(filePath);
+
+      res.sendFile(filePath);
+
+      return;
+    } else {
+      const url = await getFile(req.params.fileName);
+      const response = await axios.get(url, { responseType: "stream" });
+      // save the file to the server
+      const file = fs.createWriteStream(req.params.fileName);
+      response.data.pipe(file);
+      res.setHeader("Content-Type", response.headers["content-type"]);
+      response.data.pipe(res);
+    }
+
+    // const url = await getFile(req.params.fileName);
+
+    // const response = await axios.get(url, { responseType: "stream" });
+
+    // // save the file to the server
+    // const file = fs.createWriteStream(req.params.fileName + ".mp4");
+    // response.data.pipe(file);
+
+    // res.setHeader("Content-Type", response.headers["content-type"]);
+    // response.data.pipe(res);
   } catch (error) {
     console.log(error);
     res.status(500).send("Error getting file");
